3d4a96015abadbd5cc6adf661a1774e1342bc161
lobachaos <zwolfsasa@gmail.com>
2021-09-23 12:29:13 -0400

    Calculadora Java

---
+++ Calculadora/src/calculadora/modelo/MemoriaObservador.java
@@ -0,0 +1,7 @@
+package calculadora.modelo;
+
+@FunctionalInterface
+public interface MemoriaObservador {
+
+	 public void valorAlterado(String novoValor);
+}
---
+++ Calculadora/src/calculadora/modelo/TipoComando.java
@@ -0,0 +1,5 @@
+package calculadora.modelo;
+
+public enum TipoComando {
+	ZERAR, NUMERO , DIV , MULT , SUB , SOMA , IGUAL , VIRGULA ;
+}
